07998dc52c75fdfbe05d540b4ee133c06f4907bd
Miguel Moreno Velásquez <m0rgan@users.noreply.github.com>
2023-11-30 18:24:31 +0100

    Add files via upload

---
+++ c05/main_ex01.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_ex01.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 11:55:13 by migumore          #+#    #+#             */
+/*   Updated: 2023/11/28 15:33:28 by migumore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb);
+
+int	main(void)
+{
+	printf("%i\n", ft_recursive_factorial(5));
+	return (0);
+}
---
+++ c05/main_ex03.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_ex03.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 11:55:13 by migumore          #+#    #+#             */
+/*   Updated: 2023/11/29 11:41:41 by migumore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power);
+
+int	main(void)
+{
+	printf("%i\n", ft_recursive_power(4, 5));
+	return (0);
+}
