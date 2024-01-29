401c8f3861dd1fc887efee6bde2e6001183e2bb5
Miguel Moreno Velasquez <migumore@student.42madrid.com>
2024-01-29 12:41:33 +0100

    main lstmap

--- cursus/libft/mains/main_ft_lstmap.c
+++ cursus/libft/mains/main_ft_lstmap.c
@@ -6,19 +6,19 @@
 /*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 12:24:28 by migumore          #+#    #+#             */
-/*   Updated: 2024/01/29 12:33:03 by migumore         ###   ########.fr       */
+/*   Updated: 2024/01/29 12:41:04 by migumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-void	*(*f)(void *)
+void	*(*f)(void *list)
 {
 	*list->content += 5;
 }
 
-void	(*del)(void *)
+void	(*del)(void *list)
 {
 	free(list);
 }
@@ -27,7 +27,10 @@ int	main(void)
 {
 	t_list	*list;
 
+	list = malloc(sizeof(t_list));
+	if (list == NULL)
+		return (NULL);
 	list->content = 1;
-	printf("%i", ft_lstmap(list, f(), del()));
+	printf("%i", ft_lstmap(list, f, del));
 	return (0);
 }
