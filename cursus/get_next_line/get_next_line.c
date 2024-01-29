200d886cf6bac177c170936bc6095fa47dff6c81
Miguel Moreno V. <miguelmoreno93@gmail.com>
2024-01-29 11:27:57 +0100

    avance gnl

--- cursus/get_next_line/get_next_line.c
+++ cursus/get_next_line/get_next_line.c
@@ -6,13 +6,34 @@
 /*   By: migumore <migumore@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 19:53:11 by migumore          #+#    #+#             */
-/*   Updated: 2024/01/26 20:02:29 by migumore         ###   ########.fr       */
+/*   Updated: 2024/01/29 11:27:01 by migumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd)
 {
+	char	c;
+	size_t	bytes;
+	char	*line;
+	int	i;
 
+	bytes = read(fd, &c, 1);
+	while (c != '\n')
+		bytes += read(fd, &c, 1);
+	if (bytes == -1)
+		return (NULL);
+	line = (char *)malloc(sizeof(char) * bytes + 1);
+	i = 0;
+	while (i < bytes)
+	{
+		read(fd, &c, 1);
+		line[i] = c;
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
--- cursus/get_next_line/get_next_line.h
+++ cursus/get_next_line/get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: migumore <migumore@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 20:02:43 by migumore          #+#    #+#             */
-/*   Updated: 2024/01/26 20:04:54 by migumore         ###   ########.fr       */
+/*   Updated: 2024/01/28 13:53:14 by migumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
