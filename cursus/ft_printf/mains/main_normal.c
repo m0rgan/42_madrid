436ecdd7f0af22d38b6c640212cfaf7d40a423e5
Miguel Moreno Velasquez <migumore@student.42madrid.com>
2024-02-15 16:07:50 +0100

    ft_printf Ok

--- cursus/ft_printf/mains/main_normal.c
+++ cursus/ft_printf/mains/main_normal.c
@@ -6,14 +6,20 @@
 /*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:22:08 by migumore          #+#    #+#             */
-/*   Updated: 2024/02/14 16:22:29 by migumore         ###   ########.fr       */
+/*   Updated: 2024/02/15 15:54:57 by migumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int    main(void)
 {
-    ft_printf("Hello, %s\n%c%", "world", 'M');
+	char *s = NULL;
+	int i = 0;
+    i = ft_printf("Hello, %s\n%c", s, 'M');
+	printf("\n%d\n", i);
+	i = printf("Hello, %s\n%c", s, 'M');
+	printf("\n%d\n", i);
     return (0);
 }
