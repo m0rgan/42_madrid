649dd20d7d674709cb86b396e868858e6ed18c8f
Miguel Moreno Velasquez <migumore@student.42madrid.com>
2024-03-26 16:14:48 +0100

    mejora

--- cursus/rank_2/pipex/pipex/bonus/wait_pids_bonus.c
+++ cursus/rank_2/pipex/pipex/bonus/wait_pids_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wait_pids.c                                        :+:      :+:    :+:   */
+/*   wait_pids_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 17:03:26 by migumore          #+#    #+#             */
-/*   Updated: 2024/03/21 17:03:47 by migumore         ###   ########.fr       */
+/*   Updated: 2024/03/26 14:39:08 by migumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	wait_pids(t_pipex *data, int i)
 {
-	if (i == data->num_commands - 1)
-		waitpid(data->pids[i], &data->status, 0);
-	else
-		waitpid(data->pids[i], NULL, 0);
+	while (i < data->num_commands)
+	{
+		if (i == data->num_commands - 1)
+			waitpid(data->pids[i], &data->status, 0);
+		else
+			waitpid(data->pids[i], NULL, 0);
+		i++;
+	}
 }
